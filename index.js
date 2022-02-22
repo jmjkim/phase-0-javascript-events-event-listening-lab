4358cc53ee23d440816757a7d27503118a398f32
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-21 17:19:17 -0800

    commit tree 31832d24ff2c9558165e064c878ae47e3c6ec1ef

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+function addingEventListener(){
+    const input = document.getElementById("input");
 
     function clickAlert() {
-        alert("I was clicked!!");
-    };
+        alert("I was clicked!");
+    }
+
     input.addEventListener('click', clickAlert);
 }
-addingEventListener();
+
+addingEventListener()
